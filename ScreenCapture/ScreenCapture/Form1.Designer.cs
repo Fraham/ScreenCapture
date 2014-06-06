76dfc61a870e4f251aa696c407eac6b7b696fbca
Graham Pye <frahamp@gmail.com>
2014-06-06 16:27:02 +0100

    Adding controls

--- ScreenCapture/ScreenCapture/Form1.Designer.cs
+++ ScreenCapture/ScreenCapture/Form1.Designer.cs
@@ -28,20 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnScreenshot = new System.Windows.Forms.Button();
+            this.btnLiveFeed = new System.Windows.Forms.Button();
+            this.picFeed = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picFeed)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnScreenshot
+            // 
+            this.btnScreenshot.Location = new System.Drawing.Point(12, 12);
+            this.btnScreenshot.Name = "btnScreenshot";
+            this.btnScreenshot.Size = new System.Drawing.Size(75, 23);
+            this.btnScreenshot.TabIndex = 0;
+            this.btnScreenshot.Text = "Screenshot";
+            this.btnScreenshot.UseVisualStyleBackColor = true;
+            // 
+            // btnLiveFeed
+            // 
+            this.btnLiveFeed.Location = new System.Drawing.Point(93, 12);
+            this.btnLiveFeed.Name = "btnLiveFeed";
+            this.btnLiveFeed.Size = new System.Drawing.Size(75, 23);
+            this.btnLiveFeed.TabIndex = 0;
+            this.btnLiveFeed.Text = "Live Feed";
+            this.btnLiveFeed.UseVisualStyleBackColor = true;
+            // 
+            // picFeed
+            // 
+            this.picFeed.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picFeed.Location = new System.Drawing.Point(12, 41);
+            this.picFeed.Name = "picFeed";
+            this.picFeed.Size = new System.Drawing.Size(927, 562);
+            this.picFeed.TabIndex = 1;
+            this.picFeed.TabStop = false;
             // 
             // frmScreenCapture
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(951, 615);
+            this.Controls.Add(this.picFeed);
+            this.Controls.Add(this.btnLiveFeed);
+            this.Controls.Add(this.btnScreenshot);
             this.Name = "frmScreenCapture";
             this.Text = "Screen Capture";
+            ((System.ComponentModel.ISupportInitialize)(this.picFeed)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnScreenshot;
+        private System.Windows.Forms.Button btnLiveFeed;
+        private System.Windows.Forms.PictureBox picFeed;
     }
 }
 

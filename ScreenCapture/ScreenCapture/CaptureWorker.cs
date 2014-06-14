e322a43e84fa278f182a8915ffa36039e0369ac3
Graham Pye <frahamp@gmail.com>
2014-06-14 21:27:59 +0100

    Making Getters and Setters

--- ScreenCapture/ScreenCapture/CaptureWorker.cs
+++ ScreenCapture/ScreenCapture/CaptureWorker.cs
@@ -1,15 +1,22 @@
-﻿namespace ScreenCapture
+﻿using System.Windows.Forms;
+
+namespace ScreenCapture
 {
     internal class CaptureWorker
     {
         private int captureWidth;
         private int captureHeight;
+        private PictureBox picBox;
 
-        // Volatile is used as hint to the compiler that this data
-        // member will be accessed by multiple threads.
-        private volatile bool _shouldStop;
+        private volatile bool shouldStop;
 
-        public CaptureWorker(int captureWidth, int captureHeight)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="captureWidth"></param>
+        /// <param name="captureHeight"></param>
+        /// <param name="picBox"></param>
+        public CaptureWorker(int captureWidth, int captureHeight, PictureBox picBox)
         {
 
         }
@@ -19,12 +26,87 @@
         /// </summary>
         public void DoCapture()
         {
-
         }
 
+        /// <summary>
+        ///
+        /// </summary>
         public void RequestStop()
         {
-            
+            shouldStop = true;
         }
+
+        #region Getters and Setters
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int CaptureWidth
+        {
+            get
+            {
+                return this.captureWidth;
+            }
+
+            set
+            {
+                if(value < 0)
+                {
+                    this.captureWidth = 0;
+                }
+                else
+                {
+                    this.captureWidth = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int CaptureHeight
+        {
+            get
+            {
+                return this.captureHeight;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    this.captureHeight = 0;
+                }
+                else
+                {
+                    this.captureHeight = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public PictureBox PicBox
+        {
+            get
+            {
+                return this.picBox;
+            }
+
+            set
+            {
+                if(value != null)
+                {
+                    //make an error message.
+                }
+                else
+                {
+                    picBox = value;
+                }
+            }
+        }
+
+#endregion
     }
 }

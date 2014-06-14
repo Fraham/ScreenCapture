8f15512854805110075cdcc197e0cef32bb97650
Graham Pye <frahamp@gmail.com>
2014-06-14 21:03:53 +0100

    Making the functions

--- ScreenCapture/ScreenCapture/CaptureWorker.cs
+++ ScreenCapture/ScreenCapture/CaptureWorker.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ScreenCapture
+﻿namespace ScreenCapture
 {
-    class CaptureWorker
+    internal class CaptureWorker
     {
+        private int captureWidth;
+        private int captureHeight;
+
+        // Volatile is used as hint to the compiler that this data
+        // member will be accessed by multiple threads.
+        private volatile bool _shouldStop;
+
+        public CaptureWorker(int captureWidth, int captureHeight)
+        {
+
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public void DoCapture()
+        {
+
+        }
+
+        public void RequestStop()
+        {
+            
+        }
     }
 }

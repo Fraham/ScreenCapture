21f38f231ac467218378ab6e1f606f28b70cb0c9
Graham Pye <frahamp@gmail.com>
2014-07-07 18:35:43 +0100

    It messed up

--- ScreenCapture/ScreenCapture/frmScreenCapture.cs
+++ ScreenCapture/ScreenCapture/frmScreenCapture.cs
@@ -58,7 +58,7 @@ namespace ScreenCapture
         private void CaptureScreen()
         {
             /*
-             * Creates a new bitmap with the width and height of the primary screen (the one with the task bar).
+             * Creates a new bitmap with the width and height of the primary screen (the one with the taskbar).
              * Then it will create a graphics from the new bitmap.
              */ 
             Bitmap bitmap = new Bitmap(Screen.PrimaryScreen.WorkingArea.Width, Screen.PrimaryScreen.WorkingArea.Height);
@@ -87,14 +87,10 @@ namespace ScreenCapture
         /// <param name="e"></param>
         private void frmScreenCapture_FormClosing(object sender, FormClosingEventArgs e)
         {
-<<<<<<< HEAD
-            workerObject.RequestStop();
-=======
             if (workerObject.Started)
             {
                 workerObject.Stop();
             }
->>>>>>> origin/SortingOutThreading
         }
     }
 }

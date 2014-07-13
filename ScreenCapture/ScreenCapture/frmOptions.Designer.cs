690375373dedd258cdd6902df5342a658d8c74ee
Graham Pye <frahamp@gmail.com>
2014-07-14 00:43:48 +0100

    Changing a few form options

--- ScreenCapture/ScreenCapture/frmOptions.Designer.cs
+++ ScreenCapture/ScreenCapture/frmOptions.Designer.cs
@@ -28,9 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmOptions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "frmOptions";
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "frmOptions";
+            this.Text = "Options";
+            this.ResumeLayout(false);
+
         }
 
         #endregion

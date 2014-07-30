fd38e28dea93db79acc0745b1499aa345e16c4a8
Graham Pye <frahamp@gmail.com>
2014-07-30 20:46:50 +0100

    Added comments to options form class

--- ScreenCapture/ScreenCapture/frmOptions.cs
+++ ScreenCapture/ScreenCapture/frmOptions.cs
@@ -14,19 +14,38 @@ namespace ScreenCapture
 
         #endregion Class Variables
 
+        #region Constructor
+
+        /// <summary>
+        /// Makes a new instance of a Options menu form.
+        /// </summary>
+        /// <param name="options">The options that are currently doing run.</param>
         public frmOptions(Options options)
         {
             UsersOptions = options;
             InitializeComponent();
         }
 
+        #endregion Constructor
+
         #region Click Events
 
+        /// <summary>
+        /// It will close the form discarding the new options.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnCancel_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
+        /// <summary>
+        /// It will save the new options.
+        /// It will check to make sure that all the new values are correct.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnOk_Click(object sender, EventArgs e)
         {
             if (validWidth() && validHeight())
@@ -39,7 +58,7 @@ namespace ScreenCapture
                 {
                     UsersOptions = new Options((int)nudWidth.Value, (int)nudHeight.Value, new Point((int)nudXSourcePoint.Value, (int)nudYSourcePoint.Value));
                 }
-                
+
                 this.DialogResult = DialogResult.OK;
             }
             else
@@ -49,12 +68,22 @@ namespace ScreenCapture
             }
         }
 
+        /// <summary>
+        /// It will only close when the values are correct.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void frmOptions_FormClosing(object sender, FormClosingEventArgs e)
         {
             if (this.DialogResult == DialogResult.None)
                 e.Cancel = true;
         }
 
+        /// <summary>
+        /// Will make the capture options enabled or disabled depending whether full-screen is on or off.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void radNotFullScreen_CheckedChanged(object sender, EventArgs e)
         {
             if (radFullScreen.Checked)
@@ -66,10 +95,17 @@ namespace ScreenCapture
                 grpCaptureOptions.Enabled = true;
             }
         }
+
         #endregion Click Events
 
         #region Form Loading
 
+        /// <summary>
+        /// When the form loads.
+        /// It will set the limits on the options numeric up and downs.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void frmOptions_Load(object sender, EventArgs e)
         {
             Rectangle totalSize = Rectangle.Empty;
@@ -99,6 +135,10 @@ namespace ScreenCapture
 
         #region Validation Checks
 
+        /// <summary>
+        /// It will check if its a valid height.
+        /// </summary>
+        /// <returns>If the height and the source point are less than the max height.</returns>
         private bool validHeight()
         {
             if (nudHeight.Value + nudYSourcePoint.Value > maxHeight)
@@ -109,6 +149,10 @@ namespace ScreenCapture
             return true;
         }
 
+        /// <summary>
+        /// It will check if its a valid width.
+        /// </summary>
+        /// <returns>If the width and the source point are less than the max width.</returns>
         private bool validWidth()
         {
             if (nudWidth.Value + nudXSourcePoint.Value > maxWidth)
@@ -123,6 +167,9 @@ namespace ScreenCapture
 
         #region Properties
 
+        /// <summary>
+        /// Holds all the options for the capture
+        /// </summary>
         public Options UsersOptions
         {
             get

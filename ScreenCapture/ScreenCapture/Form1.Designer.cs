aa3b82abda8938b4c90691f07871414630d1d968
Graham Pye <frahamp@gmail.com>
2014-08-07 02:30:22 +0100

    Added menu strip

--- ScreenCapture/ScreenCapture/Form1.Designer.cs
+++ ScreenCapture/ScreenCapture/Form1.Designer.cs
@@ -32,12 +32,23 @@
             this.btnScreenshot = new System.Windows.Forms.Button();
             this.btnLiveFeed = new System.Windows.Forms.Button();
             this.picFeed = new System.Windows.Forms.PictureBox();
-            this.btnOptions = new System.Windows.Forms.Button();
             this.cmsPictureBox = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnOptions = new System.Windows.Forms.Button();
+            this.mnsScreenCapture = new System.Windows.Forms.MenuStrip();
+            this.screenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.takeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.liveFeedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pauseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.picFeed)).BeginInit();
             this.cmsPictureBox.SuspendLayout();
+            this.mnsScreenCapture.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnScreenshot
@@ -73,6 +84,28 @@
             this.picFeed.TabIndex = 1;
             this.picFeed.TabStop = false;
             // 
+            // cmsPictureBox
+            // 
+            this.cmsPictureBox.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.cmsPictureBox.Name = "cmsPictureBox";
+            this.cmsPictureBox.Size = new System.Drawing.Size(103, 48);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(102, 22);
+            this.copyToolStripMenuItem.Text = "Copy";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(102, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
             // btnOptions
             // 
             this.btnOptions.Location = new System.Drawing.Point(174, 12);
@@ -83,43 +116,100 @@
             this.btnOptions.UseVisualStyleBackColor = true;
             this.btnOptions.Click += new System.EventHandler(this.btnOptions_Click);
             // 
-            // cmsPictureBox
+            // mnsScreenCapture
             // 
-            this.cmsPictureBox.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.copyToolStripMenuItem,
-            this.saveToolStripMenuItem});
-            this.cmsPictureBox.Name = "cmsPictureBox";
-            this.cmsPictureBox.Size = new System.Drawing.Size(103, 48);
+            this.mnsScreenCapture.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.screenToolStripMenuItem,
+            this.liveFeedToolStripMenuItem,
+            this.optionsToolStripMenuItem});
+            this.mnsScreenCapture.Location = new System.Drawing.Point(0, 0);
+            this.mnsScreenCapture.Name = "mnsScreenCapture";
+            this.mnsScreenCapture.Size = new System.Drawing.Size(951, 24);
+            this.mnsScreenCapture.TabIndex = 3;
+            this.mnsScreenCapture.Text = "menuStrip1";
             // 
-            // saveToolStripMenuItem
+            // screenToolStripMenuItem
             // 
-            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.saveToolStripMenuItem.Text = "Save";
-            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            this.screenToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.takeToolStripMenuItem,
+            this.saveToolStripMenuItem1,
+            this.copyToolStripMenuItem1});
+            this.screenToolStripMenuItem.Name = "screenToolStripMenuItem";
+            this.screenToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.screenToolStripMenuItem.Text = "Screenshot";
             // 
-            // copyToolStripMenuItem
+            // takeToolStripMenuItem
             // 
-            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
-            this.copyToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.copyToolStripMenuItem.Text = "Copy";
-            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            this.takeToolStripMenuItem.Name = "takeToolStripMenuItem";
+            this.takeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.takeToolStripMenuItem.Text = "Take";
+            // 
+            // saveToolStripMenuItem1
+            // 
+            this.saveToolStripMenuItem1.Name = "saveToolStripMenuItem1";
+            this.saveToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem1.Text = "Save";
+            // 
+            // copyToolStripMenuItem1
+            // 
+            this.copyToolStripMenuItem1.Name = "copyToolStripMenuItem1";
+            this.copyToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.copyToolStripMenuItem1.Text = "Copy";
+            // 
+            // liveFeedToolStripMenuItem
+            // 
+            this.liveFeedToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.pauseToolStripMenuItem,
+            this.stopToolStripMenuItem});
+            this.liveFeedToolStripMenuItem.Name = "liveFeedToolStripMenuItem";
+            this.liveFeedToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.liveFeedToolStripMenuItem.Text = "Live Feed";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            // 
+            // pauseToolStripMenuItem
+            // 
+            this.pauseToolStripMenuItem.Name = "pauseToolStripMenuItem";
+            this.pauseToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.pauseToolStripMenuItem.Text = "Pause";
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.stopToolStripMenuItem.Text = "Stop";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "Options";
             // 
             // frmScreenCapture
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(951, 615);
+            this.Controls.Add(this.mnsScreenCapture);
             this.Controls.Add(this.picFeed);
             this.Controls.Add(this.btnOptions);
             this.Controls.Add(this.btnLiveFeed);
             this.Controls.Add(this.btnScreenshot);
+            this.MainMenuStrip = this.mnsScreenCapture;
             this.Name = "frmScreenCapture";
             this.Text = "Screen Capture";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmScreenCapture_FormClosing);
             ((System.ComponentModel.ISupportInitialize)(this.picFeed)).EndInit();
             this.cmsPictureBox.ResumeLayout(false);
+            this.mnsScreenCapture.ResumeLayout(false);
+            this.mnsScreenCapture.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -132,6 +222,16 @@
         private System.Windows.Forms.ContextMenuStrip cmsPictureBox;
         private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip mnsScreenCapture;
+        private System.Windows.Forms.ToolStripMenuItem screenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem takeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem liveFeedToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pauseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
     }
 }
 

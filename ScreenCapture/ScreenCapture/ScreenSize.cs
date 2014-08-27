8e56399c11904e16568b42bf5efebbb8f9637bc0
Graham Pye <frahamp@gmail.com>
2014-08-27 20:28:43 +0100

    Added a function which finds the top left corner position

--- ScreenCapture/ScreenCapture/ScreenSize.cs
+++ ScreenCapture/ScreenCapture/ScreenSize.cs
@@ -5,6 +5,38 @@ namespace ScreenCapture
 {
     internal class ScreenSize
     {
+        public static Point TopLeftPoint
+        {
+            get
+            {
+                Point TopLeftPoint = new Point();
+                Rectangle oldRectangle = Rectangle.Empty;
+                Rectangle newRectangle = Rectangle.Empty;
+
+                foreach (Screen s in Screen.AllScreens)
+                {
+                    if (s.Equals(Screen.PrimaryScreen))
+                    {
+                        break;
+                    }
+
+                    newRectangle = oldRectangle = Rectangle.Union(oldRectangle, s.Bounds);
+                }
+
+                if (newRectangle.Width > Screen.PrimaryScreen.WorkingArea.Width)
+                {
+                    TopLeftPoint.X = -newRectangle.Width;
+                }
+
+                if (newRectangle.Height > Screen.PrimaryScreen.WorkingArea.Height)
+                {
+                    TopLeftPoint.Y = -newRectangle.Height;
+                }
+
+                return TopLeftPoint;
+            }
+        }
+
         public static int Height
         {
             get

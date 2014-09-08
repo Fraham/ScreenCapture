fec868d0542f9f9ff2a89b24e1fada08da7e08a6
Fraham <frahamp@gmail.com>
2014-09-08 17:08:57 +0100

    Cleaned up frmUserCaptureArea

--- ScreenCapture/ScreenCapture/Options/frmUserCaptureArea.cs
+++ ScreenCapture/ScreenCapture/Options/frmUserCaptureArea.cs
@@ -6,96 +6,28 @@ namespace ScreenCapture
 {
     public partial class frmUserCaptureArea : Form
     {
-        #region:::::::::::::::::::::::::::::::::::::::::::Form level declarations:::::::::::::::::::::::::::::::::::::::::::
+        #region Form Declared
 
-        public enum CursPos : int
-        {
-            WithinSelectionArea = 0,
-            OutsideSelectionArea,
-            TopLine,
-            BottomLine,
-            LeftLine,
-            RightLine,
-            TopLeft,
-            TopRight,
-            BottomLeft,
-            BottomRight
-        }
-
-        public enum ClickAction : int
-        {
-            NoClick = 0,
-            Dragging,
-            Outside,
-            TopSizing,
-            BottomSizing,
-            LeftSizing,
-            TopLeftSizing,
-            BottomLeftSizing,
-            RightSizing,
-            TopRightSizing,
-            BottomRightSizing
-        }
-
-        public ClickAction CurrentAction;
-        public bool LeftButtonDown = false;
-        public bool RectangleDrawn = false;
-        public bool ReadyToDrag = false;
+        #region Form Variables
 
         public Point ClickPoint = new Point();
-        public Point CurrentTopLeft = new Point();
+        public ClickAction CurrentAction;
         public Point CurrentBottomRight = new Point();
+        public Point CurrentTopLeft = new Point();
         public Point DragClickRelative = new Point();
-
+        public bool LeftButtonDown = false;
+        public bool ReadyToDrag = false;
+        public bool RectangleDrawn = false;
         public int RectangleHeight = new int();
         public int RectangleWidth = new int();
-
-        private Graphics g;
-        private Pen MyPen = new Pen(Color.Red, 1);
         private Pen EraserPen = new Pen(Color.FromArgb(0, 0, 0), 20);
-
+        private Graphics g;
+        private Form m_InstanceRef = null;
+        private Pen MyPen = new Pen(Color.Red, 1);
         private Options options;
         private bool show = false;
 
-        protected override void OnMouseClick(MouseEventArgs e)
-        {
-            if (e.Button == MouseButtons.Right)
-            {
-                e = null;
-            }
-
-            base.OnMouseClick(e);
-        }
-
-        private Form m_InstanceRef = null;
-
-        public Form InstanceRef
-        {
-            get
-            {
-                return m_InstanceRef;
-            }
-            set
-            {
-                m_InstanceRef = value;
-            }
-        }
-
-        public Options CaptureOptions
-        {
-            get
-            {
-                return options;
-            }
-            set
-            {
-                options = value;
-            }
-        }
-
-        #endregion
-
-        #region:::::::::::::::::::::::::::::::::::::::::::Mouse Event Handlers & Drawing Initialization:::::::::::::::::::::::::::::::::::::::::::
+        #endregion Form Variables
 
         public frmUserCaptureArea(Options options, bool show)
         {
@@ -113,7 +45,204 @@ namespace ScreenCapture
             this.show = show;
         }
 
-        #endregion
+        #endregion Form Declared
+
+        #region Enums
+
+        public enum ClickAction : int
+        {
+            NoClick = 0,
+            Dragging,
+            Outside,
+            TopSizing,
+            BottomSizing,
+            LeftSizing,
+            TopLeftSizing,
+            BottomLeftSizing,
+            RightSizing,
+            TopRightSizing,
+            BottomRightSizing
+        }
+
+        public enum CursPos : int
+        {
+            WithinSelectionArea = 0,
+            OutsideSelectionArea,
+            TopLine,
+            BottomLine,
+            LeftLine,
+            RightLine,
+            TopLeft,
+            TopRight,
+            BottomLeft,
+            BottomRight
+        }
+
+        #endregion Enums
+
+        #region Properties
+
+        public Options CaptureOptions
+        {
+            get
+            {
+                return options;
+            }
+            set
+            {
+                options = value;
+            }
+        }
+
+        public Form InstanceRef
+        {
+            get
+            {
+                return m_InstanceRef;
+            }
+            set
+            {
+                m_InstanceRef = value;
+            }
+        }
+
+        #endregion Properties
+
+        #region Keys
+
+        public void key_press(object sender, KeyEventArgs e)
+        {
+            string sKey = e.KeyCode.ToString();
+
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.InstanceRef.Show();
+                this.Hide();
+
+                this.DialogResult = DialogResult.Cancel;
+            }
+            else if (sKey == "Z")
+            {
+                InvertColors();
+            }
+            else if (e.KeyCode == Keys.Enter)
+            {
+                CaptureOptions.Width = CurrentBottomRight.X - CurrentTopLeft.X;
+                CaptureOptions.Height = CurrentBottomRight.Y - CurrentTopLeft.Y;
+                CaptureOptions.SourcePoint = CurrentTopLeft;
+
+                this.InstanceRef.Show();
+                this.Hide();
+
+                this.DialogResult = DialogResult.OK;
+            }
+        }
+
+        #endregion Keys
+
+        #region Drawing
+
+        private void DragSelection(MouseEventArgs Cursor)
+        {
+            //Ensure that the rectangle stays within the bounds of the screen
+
+            //Erase the previous rectangle
+            g.DrawRectangle(EraserPen, (CurrentTopLeft.X), CurrentTopLeft.Y, RectangleWidth, RectangleHeight);
+
+            if ((Cursor.X) - DragClickRelative.X > 0 && (Cursor.X) - DragClickRelative.X + RectangleWidth < this.Width)
+            {
+                CurrentTopLeft.X = Cursor.X - DragClickRelative.X;
+                CurrentBottomRight.X = CurrentTopLeft.X + RectangleWidth;
+            }
+            else
+                //Selection area has reached the right side of the screen
+                if ((Cursor.X) - DragClickRelative.X > 0)
+                {
+                    CurrentTopLeft.X = this.Width - RectangleWidth;
+                    CurrentBottomRight.X = CurrentTopLeft.X + RectangleWidth;
+                }
+                //Selection area has reached the left side of the screen
+                else
+                {
+                    CurrentTopLeft.X = this.Left;
+                    CurrentBottomRight.X = CurrentTopLeft.X + RectangleWidth;
+                }
+
+            if (Cursor.Y - DragClickRelative.Y > 0 && Cursor.Y - DragClickRelative.Y + RectangleHeight < this.Width)
+            {
+                CurrentTopLeft.Y = Cursor.Y - DragClickRelative.Y;
+                CurrentBottomRight.Y = CurrentTopLeft.Y + RectangleHeight;
+            }
+            else
+                //Selection area has reached the bottom of the screen
+                if (Cursor.Y - DragClickRelative.Y > 0)
+                {
+                    CurrentTopLeft.Y = this.Height - RectangleHeight;
+                    CurrentBottomRight.Y = CurrentTopLeft.Y + RectangleHeight;
+                }
+                //Selection area has reached the top of the screen
+                else
+                {
+                    CurrentTopLeft.Y = 0;
+                    CurrentBottomRight.Y = CurrentTopLeft.Y + RectangleHeight;
+                }
+
+            //Draw a new rectangle
+            g.DrawRectangle(MyPen, CurrentTopLeft.X, CurrentTopLeft.Y, RectangleWidth, RectangleHeight);
+        }
+
+        private void DrawRectangle()
+        {
+            g.DrawRectangle(MyPen, (CurrentTopLeft.X), CurrentTopLeft.Y, (CurrentBottomRight.X) - (CurrentTopLeft.X), CurrentBottomRight.Y - CurrentTopLeft.Y);
+        }
+
+        private void DrawSelection(MouseEventArgs Cursor)
+        {
+            this.Cursor = Cursors.Arrow;
+
+            //Erase the previous rectangle
+            g.DrawRectangle(EraserPen, CurrentTopLeft.X, CurrentTopLeft.Y, CurrentBottomRight.X - (CurrentTopLeft.X), CurrentBottomRight.Y - CurrentTopLeft.Y);
+
+            //Calculate X Coordinates
+            if (Cursor.X < ClickPoint.X)
+            {
+                CurrentTopLeft.X = Cursor.X;
+                CurrentBottomRight.X = ClickPoint.X;
+            }
+            else
+            {
+                CurrentTopLeft.X = ClickPoint.X;
+                CurrentBottomRight.X = Cursor.X;
+            }
+
+            //Calculate Y Coordinates
+            if (Cursor.Y < ClickPoint.Y)
+            {
+                CurrentTopLeft.Y = Cursor.Y;
+                CurrentBottomRight.Y = ClickPoint.Y;
+            }
+            else
+            {
+                CurrentTopLeft.Y = ClickPoint.Y;
+                CurrentBottomRight.Y = Cursor.Y;
+            }
+
+            //Draw a new rectangle
+            g.DrawRectangle(MyPen, (CurrentTopLeft.X), CurrentTopLeft.Y, (CurrentBottomRight.X) - (CurrentTopLeft.X), CurrentBottomRight.Y - CurrentTopLeft.Y);
+        }
+
+        private void frmUserCaptureArea_Shown(object sender, EventArgs e)
+        {
+            if (show)
+            {
+                CurrentTopLeft = CaptureOptions.SourcePoint;
+                CurrentBottomRight = CaptureOptions.BottomRightCorner;
+
+                RectangleDrawn = true;
+
+                DrawRectangle();
+            }
+        }
 
         private void InvertColors()
         {
@@ -142,33 +271,19 @@ namespace ScreenCapture
             g.DrawRectangle(MyPen, (CurrentTopLeft.X), CurrentTopLeft.Y, (CurrentBottomRight.X) - (CurrentTopLeft.X), CurrentBottomRight.Y - CurrentTopLeft.Y);
         }
 
-        public void key_press(object sender, KeyEventArgs e)
+        #endregion Drawing
+
+        #region Mouse
+
+        protected override void OnMouseClick(MouseEventArgs e)
         {
-            string sKey = e.KeyCode.ToString();
-
-            if (e.KeyCode == Keys.Escape)
+            if (e.Button == MouseButtons.Right)
             {
-                this.InstanceRef.Show();
-                this.Hide();
-
-                this.DialogResult = DialogResult.Cancel;
+                e = null;
             }
-            else if (sKey == "Z")
-                InvertColors();
-            else if(e.KeyCode == Keys.Enter)
-            {
-                CaptureOptions.Width = CurrentBottomRight.X - CurrentTopLeft.X;
-                CaptureOptions.Height = CurrentBottomRight.Y - CurrentTopLeft.Y;
-                CaptureOptions.SourcePoint = CurrentTopLeft;
 
-                this.InstanceRef.Show();
-                this.Hide();
-
-                this.DialogResult = DialogResult.OK;
-            }
+            base.OnMouseClick(e);
         }
-
-        #region:::::::::::::::::::::::::::::::::::::::::::Mouse Buttons:::::::::::::::::::::::::::::::::::::::::::
 
         private void mouse_Click(object sender, MouseEventArgs e)
         {
@@ -187,15 +302,6 @@ namespace ScreenCapture
                 }
             }
         }
-
-        private void mouse_Up(object sender, MouseEventArgs e)
-        {
-            RectangleDrawn = true;
-            LeftButtonDown = false;
-            CurrentAction = ClickAction.NoClick;
-        }
-
-        #endregion
 
         private void mouse_Move(object sender, MouseEventArgs e)
         {
@@ -219,6 +325,17 @@ namespace ScreenCapture
                 }
             }
         }
+
+        private void mouse_Up(object sender, MouseEventArgs e)
+        {
+            RectangleDrawn = true;
+            LeftButtonDown = false;
+            CurrentAction = ClickAction.NoClick;
+        }
+
+        #endregion Mouse
+
+        #region Cursor
 
         private CursPos CursorPosition(MouseEventArgs e)
         {
@@ -271,52 +388,6 @@ namespace ScreenCapture
 
             this.Cursor = Cursors.No;
             return CursPos.OutsideSelectionArea;
-        }
-
-        private void SetClickAction(MouseEventArgs e)
-        {
-            switch (CursorPosition(e))
-            {
-                case CursPos.BottomLine:
-                    CurrentAction = ClickAction.BottomSizing;
-                    break;
-
-                case CursPos.TopLine:
-                    CurrentAction = ClickAction.TopSizing;
-                    break;
-
-                case CursPos.LeftLine:
-                    CurrentAction = ClickAction.LeftSizing;
-                    break;
-
-                case CursPos.TopLeft:
-                    CurrentAction = ClickAction.TopLeftSizing;
-                    break;
-
-                case CursPos.BottomLeft:
-                    CurrentAction = ClickAction.BottomLeftSizing;
-                    break;
-
-                case CursPos.RightLine:
-                    CurrentAction = ClickAction.RightSizing;
-                    break;
-
-                case CursPos.TopRight:
-                    CurrentAction = ClickAction.TopRightSizing;
-                    break;
-
-                case CursPos.BottomRight:
-                    CurrentAction = ClickAction.BottomRightSizing;
-                    break;
-
-                case CursPos.WithinSelectionArea:
-                    CurrentAction = ClickAction.Dragging;
-                    break;
-
-                case CursPos.OutsideSelectionArea:
-                    CurrentAction = ClickAction.Outside;
-                    break;
-            }
         }
 
         private void ResizeSelection(MouseEventArgs Cursor)
@@ -419,106 +490,52 @@ namespace ScreenCapture
             }
         }
 
-        private void DragSelection(MouseEventArgs Cursor)
+        private void SetClickAction(MouseEventArgs e)
         {
-            //Ensure that the rectangle stays within the bounds of the screen
-
-            //Erase the previous rectangle
-            g.DrawRectangle(EraserPen, (CurrentTopLeft.X), CurrentTopLeft.Y, RectangleWidth, RectangleHeight);
-
-            if ((Cursor.X) - DragClickRelative.X > 0 && (Cursor.X) - DragClickRelative.X + RectangleWidth < this.Width)
+            switch (CursorPosition(e))
             {
-                CurrentTopLeft.X = Cursor.X - DragClickRelative.X;
-                CurrentBottomRight.X = CurrentTopLeft.X + RectangleWidth;
-            }
-            else
-                //Selection area has reached the right side of the screen
-                if ((Cursor.X) - DragClickRelative.X > 0)
-                {
-                    CurrentTopLeft.X = this.Width - RectangleWidth;
-                    CurrentBottomRight.X = CurrentTopLeft.X + RectangleWidth;
-                }
-                //Selection area has reached the left side of the screen
-                else
-                {
-                    CurrentTopLeft.X = this.Left;
-                    CurrentBottomRight.X = CurrentTopLeft.X + RectangleWidth;
-                }
+                case CursPos.BottomLine:
+                    CurrentAction = ClickAction.BottomSizing;
+                    break;
 
-            if (Cursor.Y - DragClickRelative.Y > 0 && Cursor.Y - DragClickRelative.Y + RectangleHeight < this.Width)
-            {
-                CurrentTopLeft.Y = Cursor.Y - DragClickRelative.Y;
-                CurrentBottomRight.Y = CurrentTopLeft.Y + RectangleHeight;
-            }
-            else
-                //Selection area has reached the bottom of the screen
-                if (Cursor.Y - DragClickRelative.Y > 0)
-                {
-                    CurrentTopLeft.Y = this.Height - RectangleHeight;
-                    CurrentBottomRight.Y = CurrentTopLeft.Y + RectangleHeight;
-                }
-                //Selection area has reached the top of the screen
-                else
-                {
-                    CurrentTopLeft.Y = 0;
-                    CurrentBottomRight.Y = CurrentTopLeft.Y + RectangleHeight;
-                }
+                case CursPos.TopLine:
+                    CurrentAction = ClickAction.TopSizing;
+                    break;
 
-            //Draw a new rectangle
-            g.DrawRectangle(MyPen, CurrentTopLeft.X, CurrentTopLeft.Y, RectangleWidth, RectangleHeight);
+                case CursPos.LeftLine:
+                    CurrentAction = ClickAction.LeftSizing;
+                    break;
+
+                case CursPos.TopLeft:
+                    CurrentAction = ClickAction.TopLeftSizing;
+                    break;
+
+                case CursPos.BottomLeft:
+                    CurrentAction = ClickAction.BottomLeftSizing;
+                    break;
+
+                case CursPos.RightLine:
+                    CurrentAction = ClickAction.RightSizing;
+                    break;
+
+                case CursPos.TopRight:
+                    CurrentAction = ClickAction.TopRightSizing;
+                    break;
+
+                case CursPos.BottomRight:
+                    CurrentAction = ClickAction.BottomRightSizing;
+                    break;
+
+                case CursPos.WithinSelectionArea:
+                    CurrentAction = ClickAction.Dragging;
+                    break;
+
+                case CursPos.OutsideSelectionArea:
+                    CurrentAction = ClickAction.Outside;
+                    break;
+            }
         }
 
-        private void DrawSelection(MouseEventArgs Cursor)
-        {
-            this.Cursor = Cursors.Arrow;
-
-            //Erase the previous rectangle
-            g.DrawRectangle(EraserPen, CurrentTopLeft.X, CurrentTopLeft.Y, CurrentBottomRight.X - (CurrentTopLeft.X), CurrentBottomRight.Y - CurrentTopLeft.Y);
-
-            //Calculate X Coordinates
-            if (Cursor.X < ClickPoint.X)
-            {
-                CurrentTopLeft.X = Cursor.X;
-                CurrentBottomRight.X = ClickPoint.X;
-            }
-            else
-            {
-                CurrentTopLeft.X = ClickPoint.X;
-                CurrentBottomRight.X = Cursor.X;
-            }
-
-            //Calculate Y Coordinates
-            if (Cursor.Y < ClickPoint.Y)
-            {
-                CurrentTopLeft.Y = Cursor.Y;
-                CurrentBottomRight.Y = ClickPoint.Y;
-            }
-            else
-            {
-                CurrentTopLeft.Y = ClickPoint.Y;
-                CurrentBottomRight.Y = Cursor.Y;
-            }
-
-            //Draw a new rectangle
-            g.DrawRectangle(MyPen, (CurrentTopLeft.X), CurrentTopLeft.Y, (CurrentBottomRight.X) - (CurrentTopLeft.X), CurrentBottomRight.Y - CurrentTopLeft.Y);
-        }
-
-        private void DrawRectangle()
-        {
-            g.DrawRectangle(MyPen, (CurrentTopLeft.X), CurrentTopLeft.Y, (CurrentBottomRight.X) - (CurrentTopLeft.X), CurrentBottomRight.Y - CurrentTopLeft.Y);
-        }
-
-        private void frmUserCaptureArea_Shown(object sender, EventArgs e)
-        {
-            if (show)
-            {
-                CurrentTopLeft = CaptureOptions.SourcePoint;
-                CurrentBottomRight = CaptureOptions.BottomRightCorner;
-
-                RectangleDrawn = true;
-
-                DrawRectangle();
-            }
-        }
+        #endregion Cursor
     }
 }

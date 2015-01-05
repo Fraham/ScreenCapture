59e844cdc9a19d10d1cadcaf88589b1c47c0d931
Fraham <frahamp@gmail.com>
2015-01-05 02:24:49 +0000

    Adding a feed form

---
+++ ScreenCapture/ScreenCapture/Screen Capture/frmFeed.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenCapture.Screen_Capture
+{
+    public partial class frmFeed : Form
+    {
+        public frmFeed()
+        {
+            InitializeComponent();
+        }
+    }
+}

02de2e15eea8fc5eca42fca1505cc93196e2116b
Fraham <frahamp@gmail.com>
2015-01-05 02:27:51 +0000

    Adding the controls

--- ScreenCapture/ScreenCapture/Screen Capture/frmFeed.Designer.cs
+++ ScreenCapture/ScreenCapture/Screen Capture/frmFeed.Designer.cs
@@ -28,11 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.picFeed = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picFeed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picFeed
+            // 
+            this.picFeed.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picFeed.Location = new System.Drawing.Point(12, 41);
+            this.picFeed.Name = "picFeed";
+            this.picFeed.Size = new System.Drawing.Size(949, 654);
+            this.picFeed.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picFeed.TabIndex = 2;
+            this.picFeed.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(93, 12);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 1;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(174, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // frmFeed
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "frmFeed";
+            this.ClientSize = new System.Drawing.Size(973, 707);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.picFeed);
+            this.MinimumSize = new System.Drawing.Size(20, 20);
+            this.Name = "frmFeed";
+            this.Text = "Feed";
+            ((System.ComponentModel.ISupportInitialize)(this.picFeed)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picFeed;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnStop;
     }
 }

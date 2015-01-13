4e6da301b11108a796f27eb9da20d0c5ff427a64
Fraham <frahamp@gmail.com>
2015-01-13 21:25:02 +0000

    Added the screenshot class

---
+++ ScreenCapture/ScreenCapture/Screen Capture/Screenshot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenCapture.Screen_Capture
+{
+    class Screenshot
+    {
+    }
+}

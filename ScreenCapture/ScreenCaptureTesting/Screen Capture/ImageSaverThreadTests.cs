c730169b276c1e68e1480b45e9d699676f365187
Fraham <frahamp@gmail.com>
2016-05-17 16:06:34 +0100

    Basic test file

---
+++ ScreenCapture/ScreenCaptureTesting/Screen Capture/ImageSaverThreadTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScreenCapture;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenCapture.Tests
+{
+    [TestClass()]
+    public class ImageSaverThreadTests
+    {
+
+        private ImageSaverThread ist;
+
+        [TestInitialize()]
+        public void Initialize()
+        {
+            Screenshot sh = new Screenshot(100, 100);
+            sh.Capture();
+            
+            ist = new ImageSaverThread(@"C:\\", sh.Image, 1);
+        }
+
+        [TestMethod()]
+        public void ImageSaverThreadFolderPath()
+        {
+            
+        }
+
+        [TestMethod()]
+        public void ImageSaverThreadImage()
+        {
+
+        }
+
+        [TestMethod()]
+        public void ImageSaverThreadFrameNumber()
+        {
+
+        }
+    }
+}

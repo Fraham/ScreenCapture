621536de2dc7c884722c2d031beb1dff025bce0a
Fraham <frahamp@gmail.com>
2017-01-11 19:36:38 +0000

    more coverage

--- ScreenCapture/ScreenCaptureTesting/Repositories/ScreenshotRepositories/WindowsScreenshotRepositoryTests.cs
+++ ScreenCapture/ScreenCaptureTesting/Repositories/ScreenshotRepositories/WindowsScreenshotRepositoryTests.cs
@@ -4,6 +4,7 @@ using ScreenCapture.Repositories.ScreenshotRepositories;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,13 +16,50 @@ namespace ScreenCapture.Repositories.ScreenshotRepositories.Tests
     {
         IScreenshotRepository screenshotRepository = new WindowsScreenshotRepository();
 
+        Bitmap image = new Bitmap(20, 20);
+
         [TestMethod()]
-        public void SaveTest()
+        public void WindowsScreenshotRepositoryTest_Save_Simple_Success()
         {
             string fileName = "testFile.jpeg";
-            screenshotRepository.Save(new Bitmap(20, 20), fileName: fileName);
+            screenshotRepository.Save(image, fileName: fileName);
 
-            Assert.IsNotNull(screenshotRepository.Load(fileName));
+            var loadedImage = screenshotRepository.Load(fileName);
+            Assert.IsNotNull(loadedImage);
+            loadedImage.Dispose();
+        }
+
+        [TestMethod()]
+        public void WindowsScreenshotRepositoryTest_Save_NoFileExtension_Success()
+        {
+            string fileName = "testFile1";
+            screenshotRepository.Save(image, fileName: fileName);
+
+            var loadedImage = screenshotRepository.Load(fileName + ".jpeg");
+            Assert.IsNotNull(loadedImage);
+            loadedImage.Dispose();
+        }
+
+        [TestMethod()]
+        public void WindowsScreenshotRepositoryTest_Load_NoFile_Failure()
+        {
+            string fileName = "testFile2.jpeg";
+
+            Assert.IsNull(screenshotRepository.Load(fileName));
+        }
+
+        [TestCleanup]
+        public void OptionsRepositoriesTestCleanup()
+        {
+            var somethingElse = new string[]{ "testFile.jpeg", "testFile1.jpeg" };
+
+            foreach (var fileName in somethingElse)
+            {
+                if (File.Exists(fileName))
+                {
+                    File.Delete(fileName);
+                }
+            }
         }
     }
 }
